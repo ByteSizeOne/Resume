2fbb20bf33788830f6ad63e28c3504e220ac7014
ByteSizeOne <34919213+ByteSizeOne@users.noreply.github.com>
2023-06-25 15:29:00 +0000

    update

---
+++ app.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import streamlit as st
+from PIL import Image
+
+# -- PATH SETTINGS
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assests" / "CV.pdf"
+profile_pic = current_dir / "assets" / "profile-pic.png"
+
+# -- GENERAL SETTINGS
+
+PAGE_TITLE = "Digital CV | John Doe"
+PAGE_ICON = ":wave:"
+NAME = "John Doe"
+DESCRIPTION = """
+Senior Data Analyst, assisting enterprises by supporting data driven decision-making.
+"""
+EMAIL = "johndoe@mailinator.com"
+SOCIAL_MEDIA = {
+    "Youtube": "https://www.youtube/c/codingisfun",
+    "LinkedIn": "https://linkedin.com",
+    "GitHub": "https://github.com",
+    "Twitter": "https:twitter.com",
+}
+
+PROJECTS = {
+    "🔐 Sales Dashboard - Comparing sales across three stores:": "https://youtu.be/Sb0A9i6d320",
+    "🔐 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
+    "🔐 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
+    "🔐 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt"
+}
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+st.title("Hello there!")
+
